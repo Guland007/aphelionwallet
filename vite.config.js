f5d49504f2810a4ea084dbfb6f4843ebe0cfacff
Guland007 <bondarenkoserch@gmail.com>
2025-04-06 22:27:13 +0300

    fix: set base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/aphelion-wallet/',
+  base: '/www.aphelionwallet.info/',
   plugins: [react()],
 })
