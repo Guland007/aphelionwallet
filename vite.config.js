27cf3eb73ee54383d2ef39d2b9a100b133f4ec6c
Guland007 <bondarenkoserch@gmail.com>
2025-05-01 09:10:38 +0300

    chore: set Vite base URL for public icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // все ассеты из папки public будут запрашиваться от корня вашего домена
+  // например: https://aphelionwallet.info/icons/eth.png
   base: '/',
-  plugins: [react()],
+
+  plugins: [
+    react()
+  ]
 })
